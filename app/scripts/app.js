2871d13b8a4e41bb14e99d523f2bef9836cc5dca
Juan L. Sánchez <juanlu-28a@hotmail.com>
2016-12-08 18:26:48 +0100

    feat: create project with yo angularjs

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bmFrontendApp
+ * @description
+ * # bmFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bmFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
