d33d30300b8e86f423e09fc777d250f8ad37fd12
Juan L. Sánchez <juanlu-28a@hotmail.com>
2017-01-07 16:39:44 +0100

    feat: add selected in navbar

--- app/scripts/controllers/navbar.controller.js
+++ app/scripts/controllers/navbar.controller.js
@@ -19,11 +19,25 @@
           }
           var isAuthenticated = Principal.isAuthenticated;
 
+          function listItemClass(item) {
+              var result = 'md-1-line';
+              var url = $state.$current.url.toString();
+              var itemUrl = '/' + item + '/';
+              if (url.includes(itemUrl)) {
+                  result += ' is-selected-son'
+              }
+              if ($state.$current.name == item) {
+                  result += ' is-selected'
+              }
+              return result;
+          }
+
           function init() {
               vm.nav = nav;
               vm.logout = logout;
               vm.isAuthenticated = isAuthenticated;
               vm.sidenav = sidenav;
+              vm.listItemClass = listItemClass;
           }
           init();
 
